f85d62dfaeb8e7873e2fde8dee0a8a6171f472c4
Joronski <joronrecio@yahoo.com>
2026-04-05 20:12:42 +0800

    auth-and-security: Implemented basic FirebaseAuth operations in AuthRepository

--- app/src/main/java/com/example/hanaparal/data/repository/AuthRepository.kt
+++ app/src/main/java/com/example/hanaparal/data/repository/AuthRepository.kt
@@ -1,2 +1,20 @@
 package com.example.hanaparal.data.repository
 
+import com.google.firebase.auth.AuthCredential
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+
+class AuthRepository(private val auth: FirebaseAuth) {
+    val currentUser: FirebaseUser? get() = auth.currentUser
+
+    fun signOut() {
+        auth.signOut()
+    }
+
+    fun signInWithCredential(credential: AuthCredential, onResult: (Boolean) -> Unit) {
+        auth.signInWithCredential(credential)
+            .addOnCompleteListener { task ->
+                onResult(task.isSuccessful)
+            }
+    }
+}
